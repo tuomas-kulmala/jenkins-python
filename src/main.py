5f003178019dbc0715e34e9c937a0a942271f874
Tuomas Kulmala <sufa@qscrum.com>
2018-07-09 03:44:49 +0300

    changed project structure

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 import os
-
+from future.types import bytes, dict, int, range, str
 
 class Creature(object):
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
 
     def name_upper(self):
         return str.upper(self.name)
 
+
 if __name__ == '__main__':
     c = Creature('dog')
     print(c.name_upper())
+    print(c.add(1))
